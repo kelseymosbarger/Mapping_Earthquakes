3263d8111db335415d3c7131fe10d075b20d5296
kelseymosbarger <kjmosbarger@gmail.com>
2022-03-15 12:58:57 -0600

    adding code for cities array and mupliple points on map logic

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -12,12 +12,14 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circle([34.0522, -118.2437], {
-  radius:300,
-  color:"black",
-  fillColor: '#ffffa1'
-}).addTo(map);
+// Get data from cities.js
+let cityData = cities
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.marker(city.location).addTo(map);
+ });
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
